8d92a8b8134ac2b592b385daf45d1496314d69c0
aliasgs <syd.1991@gmail.com>
2022-09-09 17:39:12 +0200

    first commit

---
+++ code.ts
@@ -0,0 +1,144 @@
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+let cover: PageNode;
+let frame: FrameNode;
+
+let bgHex = '#007CBF';
+
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+
+
+
+function createPageAndFrame() {
+  //creating page
+  cover = figma.createPage();
+  cover.name = 'Thumbnail';
+  //setting background to bgHex variable
+  cover.backgrounds = [
+      {
+          type: 'SOLID',
+          color: {
+            r: hexToRgb(bgHex).r,
+            g: hexToRgb(bgHex).g,
+            b: hexToRgb(bgHex).b,
+          },
+      },
+  ];
+      //setting current page to this new page
+      figma.currentPage = cover;
+
+      //insert new page at the top of the root (second page)
+      figma.root.insertChild(0, cover);
+
+      //creating frame
+      frame = figma.createFrame();
+      frame.name = 'Thumbnail';
+      frame.resize(1600, 960);
+      frame.fills = [
+          {
+              type: 'SOLID',
+              color: {
+                  r: hexToRgb(bgHex).r,
+                  g: hexToRgb(bgHex).g,
+                  b: hexToRgb(bgHex).b,
+              },
+          },
+      ];
+
+      //zoom into frame
+      const nodes = [];
+      nodes.push(frame);
+      figma.viewport.scrollAndZoomIntoView(nodes);
+
+}
+
+
+
+function createContent(){
+
+  (async () => {
+  
+  const text = figma.createText()
+
+  // Move to (50, 50)
+  text.x = 50
+  text.y = 50
+
+  // Load the font in the text node before setting the characters
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+  text.characters = 'Hello world!'
+
+  // Set bigger font size and red color
+  text.fontSize = 48
+  text.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }]
+
+
+})()
+
+}
+
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-frame') {
+
+    createPageAndFrame();
+    figma.notify("frame created!");
+    
+  }
+
+if(msg.type === 'create-content'){
+  createContent();
+    figma.notify("frame created!");
+
+}
+
+
+  if (msg.type === 'set-as-thumbnail') {
+
+    if(figma.currentPage.id === cover.id){
+          const template = figma.currentPage.findAllWithCriteria({
+            types: ['FRAME']
+          })
+      
+          figma.setFileThumbnailNodeAsync(template[0]);
+          figma.notify("thumbnail set!")
+
+    }else{
+      figma.notify("Go to the Thumbnail page!")
+
+    }
+
+
+  
+  }
+  
+
+
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  //figma.closePlugin();
+};
+
+
+
+
+function hexToRgb(hex: string) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+      ? {
+            r: parseInt(result[1], 16) / 255,
+            g: parseInt(result[2], 16) / 255,
+            b: parseInt(result[3], 16) / 255,
+        }
+      : null;
+}
